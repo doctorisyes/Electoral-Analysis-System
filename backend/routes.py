b1f616f5136e00191b18ef51f889e9af3e072929
justsamueld <78651799+doctorisyes@users.noreply.github.com>
2025-05-12 19:12:46 +0100

    Add Simple Flask App

---
+++ backend/routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template # Acess the Blueprint class and the render_template function from Flask
+
+mainBlueprint = Blueprint('main_blueprint', __name__) # Create an instance of the blueprint class
+
+@mainBlueprint.route('/') # Add the folowing function as the "/" page of the blurprint
+def index():
+    return render_template('index.html') # Return the rendered HTML file index.html
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # Run with virtualenv and install dependencies from requirements.txt in order to run main.py
+
+from flask import Flask # Allow python to acess Flask
+from backend.routes import mainBlueprint # Acess the routes.py file with all of the routes eg: /home etc.
+
+app = Flask(__name__) # Create a Flask app instance
+app.register_blueprint(mainBlueprint) # Register the routes from routes.py with the Flask app
+
+if __name__ == '__main__': # Only run this if this file is run directly
+    app.run(host="localhost", port=5000) # Host the app on a web server on localhost and port 5000
