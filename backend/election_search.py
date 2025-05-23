e4a78b593eade8705bc704cf6e907ec8e4eb6f40
justsamueld <78651799+doctorisyes@users.noreply.github.com>
2025-05-23 19:56:17 +0100

    CountryCode queries and Continent lists support

--- backend/election_search.py
+++ backend/election_search.py
@@ -1,18 +1,30 @@
-from api_requests import read_api_data # Import api_requests script
+from . import api_requests # Import api_requests script
 
 def getElectionById(electionId):
-    mainDb = read_api_data() # Get the dictionary of the cached API DB
+    mainDb = api_requests.read_api_data() # Get the dictionary of the cached API DB
     for election in mainDb:
         if electionId == election['election_id']: # If the id matches with the one on the DB
             return election
         
 def getElectionsByCountryCode(countryCode):
-    mainDb = read_api_data() # Get the dictionary of the cached API DB
+    mainDb = api_requests.read_api_data() # Get the dictionary of the cached API DB
     matchingElections = [] # Make an empty list
     for election in mainDb:
         if countryCode == election['district']['district_country']: # If the ditrict code matches
             matchingElections.append(election) # Add the election to the matchingElections list
     return matchingElections
 
+def refineElectionName(name, year):
+    if name[-4:] == str(year):
+        return name[:-4]
+    
+def findElectionYear(election):
+    if election["election_range_start_date"] != None:
+        return election["election_range_start_date"][:4]
+    elif election["election_declared_start_date"] != None:
+        return election["election_declared_start_date"][:4]
+    elif election["original_election_year"] != None:
+        return str(election["original_election_year"])
+
 if __name__ == "__main__":
-    print(getElectionsByCountryCode("US"))
+    print(findElectionYear(getElectionById(3970)))
